fb67986f5faa616102cbb78778840206949f8a46
thegreenn1nja <73561889+thegreenn1nja@users.noreply.github.com>
2025-09-11 21:55:46 -0500

    Wrote main
    scanner works and now it captures the stuff from the command line

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.util.Scanner;
 import java.lang.*;
+import java.util.Arrays;
 
 // This algorithm approximates a solution to the Bin Packing Problem
 // It is a way to pack 'n' items of different sizes in 3 bins with capacity 'B' each
@@ -11,21 +12,21 @@ import java.lang.*;
 
 
 public class Main {
-    public static int Bin() {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        int B = input.nextInt();
-
+        System.out.println("Enter the number of items to be packed:");
         int n = input.nextInt();
-
-        System.out.println(B);
-
-        System.out.println(n);
-        
-        int items = new int[n];
+        System.out.println("Enter the capacity of each bin:");
+        int B = input.nextInt();
+        int[] items = new int[n];
+        System.out.println("Enter the sizes of the items:");
         for (int i = 0; i < n; i++) {
+            items[i] = input.nextInt();
         }
-        input.close();  
-        return B;
+        Arrays.sort(items);
+        System.out.println("Number of items: " + n);
+        System.out.println("Bin capacity: " + B);
+        System.out.println("Items to be packed: " + Arrays.toString(items));
+        input.close();
     }
 }
