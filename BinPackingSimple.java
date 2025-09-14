31c0c1857626e277075b685530df08ebfb9a9717
Mahathir <mosta044@umn.edu>
2025-09-14 13:07:48 -0500

    Main updates
    Bin packing method created.
    
    Works but need a way to show the output, for example:
    
    Packing Result
    Bin 1 = [15, 4] (19/20)
    Bin 2 = [12, 8] (20/20)
    Bin 3 = [10, 9] (19/20)
    
    unpacked = (whichever numbers)
    
    Something like that, but needs to be created.

---
+++ BinPackingSimple.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+public class BinPackingSimple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read input
+        int B = sc.nextInt();      // bin capacity
+        int n = sc.nextInt();      // number of items
+        int[] items = new int[n];
+        for (int i = 0; i < n; i++) items[i] = sc.nextInt();
+        sc.close();
+
+        // Sort items in decreasing order
+        Arrays.sort(items);
+        for (int i = 0; i < n / 2; i++) {
+            int tmp = items[i];
+            items[i] = items[n - 1 - i];
+            items[n - 1 - i] = tmp;
+        }
+
+        // 3 bins
+        ArrayList<Integer>[] bins = new ArrayList[3];
+        int[] used = new int[3];
+        for (int i = 0; i < 3; i++) bins[i] = new ArrayList<>();
+
+        ArrayList<Integer> unpacked = new ArrayList<>();
+
+        // First-Fit Decreasing
+        for (int item : items) {
+            boolean placed = false;
+            for (int b = 0; b < 3; b++) {
+                if (used[b] + item <= B) {
+                    bins[b].add(item);
+                    used[b] += item;
+                    placed = true;
+                    break;
+                }
+            }
+
+            if (!placed) unpacked.add(item);
+        }
+
+        // Output
+        System.out.println("=== Packing Result ===");
+        for (int b = 0; b < 3; b++) {
+            System.out.println("Bin " + (b+1) + ": " + bins[b] + 
+                               " (used " + used[b] + "/" + B + ")");
+        }
+        System.out.println("Unpacked: " + unpacked);
+        int unused = 3*B - (used[0] + used[1] + used[2]);
+        System.out.println("Total unused space: " + unused);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
 import java.util.*;
-import java.util.Scanner;
-import java.lang.*;
-import java.util.Arrays;
 
 // This algorithm approximates a solution to the Bin Packing Problem
 // It is a way to pack 'n' items of different sizes in 3 bins with capacity 'B' each
@@ -14,19 +11,64 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number of items to be packed:");
+
+            System.out.println("Enter the number of items to be packed:");
+
         int n = input.nextInt();
-        System.out.println("Enter the capacity of each bin:");
+
+            System.out.println("Enter the capacity of each bin:");
+
         int B = input.nextInt();
+
         int[] items = new int[n];
-        System.out.println("Enter the sizes of the items:");
-        for (int i = 0; i < n; i++) {
+
+            System.out.println("Enter the sizes of the items:");
+
+        for (int i = 0; i < n; i++) 
             items[i] = input.nextInt();
-        }
+
+        input.close();
+    
+        //sorts in decreasing
         Arrays.sort(items);
+            for (int i = 0; i < n/2; i++) {
+                int tmp = items[i];
+                items[i] = items[n - 1 - i];
+                items[n - 1 - i] = tmp;
+
+            }    
+        
         System.out.println("Number of items: " + n);
         System.out.println("Bin capacity: " + B);
         System.out.println("Items to be packed: " + Arrays.toString(items));
         input.close();
+    
+
+    //3 bins
+
+      ArrayList<Integer>[] bins = new ArrayList[3];
+        int[] used = new int[3]; 
+        for (int i = 0; i < 3; i++) bins[i] = new ArrayList<>();
+    
+    //list for items that cant be placed into any of the bins
+
+    ArrayList<Integer> unpacked = new ArrayList<>();
+
+    //Looping through each item 
+    for( int item : items) {
+        boolean placed = false; //item not placed into a bin yet
+        for(int b = 0; b < 3; b++) {
+            if (used[b] + item <= b) {   // if item fits into bin (1 through 3)
+                bins[b].add(item); //then it adds the item into that bin (whichever one it fits into 1-3)
+                used[b] += item;
+                placed = true;
+                break; //if placed then stops the loop
+            }
+        }
+        if (!placed) unpacked.add(item); //this is to store the item if it doesnt fit into a bin
+
+        
     }
+}
+
 }
