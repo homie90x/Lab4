deab7a533d66bec542d6a8e6bc1e9da3abb46e6d
thegreenn1nja <73561889+thegreenn1nja@users.noreply.github.com>
2025-09-17 21:44:12 -0500

    Complerted lab

--- Main.java
+++ Main.java
@@ -7,20 +7,37 @@ import java.util.*;
 // Allocates them into 3 bins (N) 
 // Allocates them based on bin capacity (B)
 
+// Jakob Linscheid and Mahathir Mostafa 
+
+// Optimal 1
+// B=20 n=8 items[15 5 3 7 10 8 2 9]
+// Output: Bin 1[15,5], Bin 2[10,9], Bin 3[8,7,3,2] 
+// No unused items
+
+// Optimal 2
+// B=10 n=8 items[7 2 1 5 5 5 2 3]
+// Output: Bin 1[7,3], Bin 2[5,5], Bin 3[5,2,2,1]
+// No unused items
+
+// Non-Optimal
+// B=10 n=8 items[1 4 10 7 8 5 6 9]
+// Output: Bin 1[10], Bin 2[9,1], Bin 3[8] 
+// Unused items: 7,6,5,4
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //Allows the program to use inputs from the console.
 
             System.out.println("Enter the number of items to be packed:");
 
-        int n = input.nextInt();
+        int n = input.nextInt(); //number of items
 
             System.out.println("Enter the capacity of each bin:");
 
-        int B = input.nextInt();
+        int B = input.nextInt(); //capacity of each bin
 
-        Integer[] items = new Integer[n];
+        Integer[] items = new Integer[n]; //array to hold the sizes of the items
 
             System.out.println("Enter the sizes of the items:");
 
@@ -59,7 +76,7 @@ public class Main {
             }
 
             // Print bins and their remaining space
-            System.out.println("Packing Result:");
+            System.out.println("Packing Result:"); 
             for (int i = 0; i < binCount; i++) {
                 System.out.println("Bin " + (i+1) + ": " + bins.get(i) + " (used " + (B-binSpace[i]) + "/" + B + ")");
             }
